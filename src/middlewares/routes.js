9a58b38d4f3cf3de3723019993b22e88b0b2e1de
Daniel Akira <daakira@hotmail.com>
2023-10-20 01:19:40 -0300

    Adicionando rota POST de task

--- src/middlewares/routes.js
+++ src/middlewares/routes.js
@@ -1,17 +1,38 @@
+import { randomUUID } from "node:crypto";
 import { buildRoutePath } from "../utils/buildRoutePath.js";
 import { Database } from "./database.js";
 
-
-const database = new Database()
+const database = new Database();
 
 export const routes = [
-    {
-        method: "GET",
-        path: buildRoutePath("/task"),
-        handler: (req, res) => {
-            const { search } = req.query
+  {
+    method: "GET",
+    path: buildRoutePath("/task"),
+    handler: (req, res) => {
+      const { search } = req.query;
 
-            const tasks = 0
-        }
+      const tasks = 0;
     },
-]
+  },
+  {
+    method: "POST",
+    path: buildRoutePath("/tasks"),
+    handler: (req, res) => {
+      const { title, description } = req.body;
+      const date = new Date();
+
+      const task = {
+        id: randomUUID(),
+        title,
+        description,
+        completedAt: null,
+        createdAt: date,
+        updatedAt: date,
+      };
+
+      database.insert("tasks", task);
+
+      return res.writeHead(201).end();
+    },
+  },
+];
